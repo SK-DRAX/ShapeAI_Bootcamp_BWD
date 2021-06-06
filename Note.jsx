88bef59020ff5717c99de4b4279fd9e49c8a1a65
Swapnil Khot <74708262+SK-DRAX@users.noreply.github.com>
2021-06-06 22:25:29 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Javascript and React.Js</h1>
+        <p>
+          This was the amazing bootcamp. In this bootcamp you will get to learn
+          the basics of Javascript and React.js which will help you in frontend
+          web development.
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>Python</h1>
+        <p>
+          This was the amazing bootcamp. In this bootcamp you will get to learn
+          the basics of Python programming Language which will help you in
+          writing programs by yourself and also helps in problem solving skills.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
